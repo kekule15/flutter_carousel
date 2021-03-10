a0999fbf5ecc1dc21947bb5137d9afb731e9b3f7
praveend <praveend2622@gmail.com>
2021-03-10 21:47:51 +0530

    Signed-off-by: praveend <praveend2622@gmail.com>

---
+++ example/example.dart
@@ -0,0 +1,52 @@
+import 'package:carousel/carousel.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home();
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      height: size.height,
+      width: size.width,
+      child: Column(
+        children: [
+          Container(
+              height: size.height * 0.2,
+              width: size.width,
+              child: Carousel(
+                indicatorBarColor: Colors.black.withOpacity(0.2),
+                autoScrollDuration: Duration(seconds: 2),
+                animationPageDuration: Duration(milliseconds: 500),
+                activateIndicatorColor: Colors.black,
+                animationPageCurve: Curves.bounceInOut,
+                indicatorBarHeight: 50,
+                indicatorHeight: 20,
+                indicatorWidth: 20,
+                unActivatedIndicatorColor: Colors.grey,
+                stopAtEnd: true,
+                autoScroll: true,
+                // widgets
+                items: [
+                  Container(
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    color: Colors.green,
+                  ),
+                  Container(
+                    color: Colors.yellow,
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
+  }
+}
